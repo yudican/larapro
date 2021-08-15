38ec4a88712628340838e8354b03a696345256a2
Yudi Candra <yudhichandra02@gmail.com>
2021-08-15 18:28:37 +0700

    add menu migration

---
+++ database/migrations/2021_08_15_112441_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('menu_label', 50);
+            $table->string('menu_route', 30);
+            $table->string('menu_icon', 30);
+            $table->string('menu_order', 30);
+            $table->bigInteger('parent_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
